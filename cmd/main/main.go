34329f5745693c8a52c0161011bf6136c52cea49
Ngeo Jia Jun <ngeojiajun@gmail.com>
2025-09-11 16:04:31 +0000

    cmd: tidy up the command line tools

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,46 +2,61 @@ package main
 
 // File: cmd/main/main.go
 // A small demo program to show its functionality
-// TODO: clean this up
 
 import (
 	"encoding/hex"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
-	container "github.com/ngeojiajun/go-filecrypt/pkg/container"
+	"github.com/ngeojiajun/go-filecrypt/pkg/container"
 	types "github.com/ngeojiajun/go-filecrypt/pkg/types"
 )
 
-type config struct {
-	encrypt   bool
-	decrypt   bool
-	overwrite bool
-	key       []byte
-	from      string
-	to        string
+type Config struct {
+	Overwrite bool
+	Key       []byte
+	From      string
+	To        string
 }
 
-func parseFlags() (*config, error) {
+// Parse the flag
+func parseFlags(cmd string, arg []string) (*Config, error) {
 	// Define flags
-	encrypt := flag.Bool("encrypt", false, "Perform encryption")
-	decrypt := flag.Bool("decrypt", false, "Perform decryption")
-	overwrite := flag.Bool("overwrite", false, "Overwrite file if exists")
-	keyHex := flag.String("key", "", "Hex-encoded key")
-	from := flag.String("from", "", "Input file path")
-	to := flag.String("to", "", "Output file path")
-	flag.Parse()
+	flagSet := flag.NewFlagSet(cmd, flag.ContinueOnError)
+	overwrite := flagSet.Bool("overwrite", false, "Overwrite file if exists")
+	keyHex := flagSet.String("key", "", "Hex-encoded key")
+	from := flagSet.String("from", "", "Input file path")
+	to := flagSet.String("to", "", "Output file path")
+	flagSet.Usage = func() {
+		o := flagSet.Output()
+		fmt.Fprintf(o, "Usage:\n %s %s [options] [from] [to]\n\n", os.Args[0], cmd)
+		flagSet.PrintDefaults()
+		fmt.Fprint(o, "\n The -from and -to are mutually exclusive with the positional arguments\n")
+	}
+	if len(arg) == 0 {
+		return nil, flagSet.Parse([]string{"-h"})
+	}
+
+	if err := flagSet.Parse(arg); err != nil {
+		return nil, err
+	}
+
+	// Collect positional parameters
+	if flagSet.NArg() > 0 {
+		if *from != "" || *to != "" {
+			return nil, fmt.Errorf("-from and -to are mutually exclusive with the positional arguments")
+		}
+		*from = flagSet.Arg(0)
+		*to = flagSet.Arg(1)
+		if flagSet.NArg() > 2 {
+			return nil, fmt.Errorf("too many positional arguments: %v", flagSet.Args()[2:])
+		}
+	}
 
 	// Validate flags
-	if !*encrypt && !*decrypt {
-		return nil, fmt.Errorf("must specify either -encrypt or -decrypt")
-	}
-	if *encrypt && *decrypt {
-		return nil, fmt.Errorf("-encrypt and -decrypt are mutually exclusive")
-	}
 	if *keyHex == "" {
 		return nil, fmt.Errorf("missing required -key")
 	}
@@ -62,95 +77,119 @@ func parseFlags() (*config, error) {
 			2*types.SlotKeyAlgAESGCM128.KeySize())
 	}
 
-	return &config{
-		encrypt:   *encrypt,
-		decrypt:   *decrypt,
-		key:       key,
-		overwrite: *overwrite,
-		from:      *from,
-		to:        *to,
+	return &Config{
+		Key:       key,
+		Overwrite: *overwrite,
+		From:      *from,
+		To:        *to,
 	}, nil
 }
 
-func fileExists(path string) (bool, error) {
-	info, err := os.Lstat(path)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return false, nil
-		}
-		return false, err
-	}
-	return info.Mode().IsRegular(), nil
+func showQuickUsage() {
+	log.Fatalf("Usage: %s [encrypt|decrypt] [additional options.....]\n", os.Args[0])
 }
 
 // A simple program to process the stuffs
 func main() {
-	cfg, err := parseFlags()
+	var operation func(conf *Config) error
+	if len(os.Args) == 1 {
+		showQuickUsage()
+	}
+	cmd := os.Args[1]
+	switch cmd {
+	case "encrypt":
+		operation = ProcessEncryption
+	case "decrypt":
+		operation = ProcessDecryption
+	case "help", "-h", "--help":
+		showQuickUsage()
+	default:
+		log.Fatalf("Unknown subcommand: %s", cmd)
+	}
+
+	cfg, err := parseFlags(cmd, os.Args[2:])
 	if err != nil {
 		log.Fatal(err)
 	}
-	if exists, err := fileExists(cfg.from); err != nil {
+	if exists, err := FileExists(cfg.From); err != nil {
 		log.Fatalf("IO error happened: %v", err)
 	} else if !exists {
-		log.Fatalf("%s does not exists", cfg.from)
+		log.Fatalf("%s does not exists", cfg.From)
 	}
-	if exists, err := fileExists(cfg.to); err != nil {
+	if exists, err := FileExists(cfg.To); err != nil {
 		log.Fatalf("IO error happened: %v", err)
-	} else if exists && !cfg.overwrite {
-		log.Fatalf("%s already exists, use -overwrite to overwrite the file", cfg.to)
+	} else if exists && !cfg.Overwrite {
+		log.Fatalf("%s already exists, use -overwrite to overwrite the file", cfg.To)
 	}
-	var fileContainer *container.ContainerFile
-	if cfg.encrypt {
-		fileContainer, err = container.NewContainerFile(cfg.to, types.EncAlgAESCTR256)
-		if err != nil {
-			log.Fatalf("IO error happened, while creating the file: %v", err)
-		}
-		err = fileContainer.AddKeySlot(types.SlotKeyAlgAESGCM128, cfg.key)
-		if err == nil {
-			err = fileContainer.WriteHeader()
-		}
-		if err != nil {
-			fileContainer.Close()
-			os.Remove(cfg.to)
-			log.Fatalf("Cannot prepare the file: %v", err)
-		}
-	} else {
-		fileContainer, err = container.OpenContainerFile(cfg.from)
-		if err != nil {
-			log.Fatalf("Error happened, while opening the file: %v", err)
-		}
-		err = fileContainer.Unseal(types.SlotKeyAlgAESGCM128, cfg.key)
-		if err != nil {
-			log.Fatalf("Error happened, while opening the file: %v", err)
-		}
-	}
-	defer (func() {
-		fileContainer.Close()
-		if err != nil {
-			os.Remove(cfg.to)
-		}
-	})()
 
-	// Open the plaintext file
-	var plaintext *os.File
-	if cfg.encrypt {
-		plaintext, err = os.Open(cfg.from)
-		if err != nil {
-			log.Fatalf("IO error happened, while creating the file (%s): %v", cfg.from, err)
-		}
-		defer plaintext.Close() // Auto close it
-		err = fileContainer.EncryptStream(plaintext)
-	} else {
-		plaintext, err = os.Create(cfg.to)
-		if err != nil {
-			log.Fatalf("IO error happened, while creating the file (%s): %v", cfg.to, err)
-		}
-		defer plaintext.Close() // Auto close it
-		err = fileContainer.DecryptStream(plaintext)
+	absFrom, _ := filepath.Abs(cfg.From)
+	absTo, _ := filepath.Abs(cfg.To)
+	if absFrom == absTo {
+		log.Fatalf("from and to must be different paths")
 	}
+
+	err = operation(cfg)
+
 	if err == nil {
 		log.Print("Done")
 	} else {
 		log.Fatalf("Error happened: %v", err)
 	}
+}
+
+func ProcessEncryption(cfg *Config) error {
+	fileContainer, err := container.NewContainerFile(cfg.To, types.EncAlgAESCTR256)
+	if err != nil {
+		return fmt.Errorf("IO error happened, while creating the file: %v", err)
+	}
+	err = fileContainer.AddKeySlot(types.SlotKeyAlgAESGCM128, cfg.Key)
+	if err == nil {
+		err = fileContainer.WriteHeader()
+	}
+	if err != nil {
+		fileContainer.Close()
+		os.Remove(cfg.To)
+		return fmt.Errorf("cannot prepare the file: %v", err)
+	}
+	defer (func() {
+		fileContainer.Close()
+		if err != nil {
+			os.Remove(cfg.To)
+		}
+	})()
+
+	// Open the plaintext file
+	plaintext, err := os.Open(cfg.From)
+	if err != nil {
+		return fmt.Errorf("IO error happened, while creating the file (%s): %v", cfg.From, err)
+	}
+	defer plaintext.Close() // Auto close it
+	err = fileContainer.EncryptStream(plaintext)
+	return err
+}
+
+func ProcessDecryption(cfg *Config) error {
+	fileContainer, err := container.OpenContainerFile(cfg.From)
+	if err != nil {
+		return fmt.Errorf("error happened, while opening the file: %v", err)
+	}
+	err = fileContainer.Unseal(types.SlotKeyAlgAESGCM128, cfg.Key)
+	if err != nil {
+		return fmt.Errorf("error happened, while unsealing the file: %v", err)
+	}
+	defer (func() {
+		fileContainer.Close()
+		if err != nil {
+			os.Remove(cfg.To)
+		}
+	})()
+
+	// Open the plaintext file
+	plaintext, err := os.Create(cfg.To)
+	if err != nil {
+		return fmt.Errorf("IO error happened, while creating the file (%s): %v", cfg.To, err)
+	}
+	defer plaintext.Close() // Auto close it
+	err = fileContainer.DecryptStream(plaintext)
+	return err
 }
