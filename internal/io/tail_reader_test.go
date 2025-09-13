7bc0e25b66842d0db550d96d6b8fa269d8dc6031
Ngeo Jia Jun <ngeojiajun@gmail.com>
2025-09-13 03:45:56 +0000

    test: Add benchmark for some internal tools

---
+++ internal/io/tail_reader_test.go
@@ -0,0 +1,60 @@
+package io_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+
+	_io "github.com/ngeojiajun/go-filecrypt/internal/io"
+	"github.com/ngeojiajun/go-filecrypt/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCorrectNess(t *testing.T) {
+	testPayload, err := utils.GenerateRandomBytes(60 * 4096) // 60 pages
+	assert.NoError(t, err, "cannot generate random bytes for testing")
+	reader := bytes.NewReader(testPayload)
+	tailReader := _io.NewTailReader(reader, 64)
+	tmp := make([]byte, 1028) // 0.5 pages
+	idx := 0
+	round := 0
+	for err == nil {
+		round += 1
+		t.Logf("Round %d", round)
+		var n int
+		n, err = tailReader.Read(tmp)
+		if n > 0 {
+			assert.Equal(t, testPayload[idx:idx+n], tmp[:n])
+			idx += n
+		}
+	}
+	assert.ErrorIs(t, err, io.EOF)
+	tail, err := tailReader.Tail()
+	assert.NoError(t, err, "cannot read tail")
+	assert.Equal(t, testPayload[idx:], tail)
+}
+
+func BenchmarkTailReader(b *testing.B) {
+	testPayload, err := utils.GenerateRandomBytes(60 * 4096) // 60 pages
+	assert.NoError(b, err, "cannot generate random bytes for testing")
+	bufSizes := []int{1024, 2048, 4096, 8192}
+	for _, size := range bufSizes {
+		b.Run(fmt.Sprintf("Buffer Size = %d", size), func(b *testing.B) {
+			tmp := make([]byte, 1024) // 0.25 pages
+			idx := 0
+			b.ResetTimer()
+			for b.Loop() {
+				reader := bytes.NewReader(testPayload)
+				tailReader := _io.NewTailReader(reader, 64)
+				for err == nil {
+					var n int
+					n, err = tailReader.Read(tmp)
+					if n > 0 {
+						idx += n
+					}
+				}
+			}
+		})
+	}
+}
